297e07e274fdf234b3c6f4b869bdce5b7f3a9a99
Aleksandrs Belikovs Air <alekss@enter7.lv>
2019-11-29 01:31:33 +0200

    Final move. Default numbers update, innit element updates, JS update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,127 +1,152 @@
 console.log('My script started');
 
-//jquery smooth animation
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 400, function(){
-        window.location.hash = hash;
-      });
-    }
-  });
-});
+// Start values
 
-//start values
 startValue.value = 1;
 endValue.value = 100;
 fizzValue.value = 3;
 buzzValue.value = 5;
 
 function innit () {
-  //  console.log('Adding listeners')
-  addListeners();
-  document.getElementById('elements');
+    addListeners();
+    document.getElementById('elements');
 
-  var clearButton = document.getElementById('clear');
-  clearButton.addEventListener("click", onClickClear);
-  var submitCustom = document.getElementById('generate2');
-  submitCustom.addEventListener("click", onClickCustom);
+    const clearButton = document.getElementById('reset');
+    clearButton.addEventListener("click", onClickReset);
+    const submitCustom = document.getElementById('generate__custom');
+    submitCustom.addEventListener("click", onClickCustom);
+    const submit = document.getElementById('generate');
+    submit.addEventListener("click", onClick);
+
+    clearButton.onclick = function() {
+        startValue.value = 1;
+        endValue.value = 100;
+        fizzValue.value = 3;
+        buzzValue.value = 5;
+        innit();
+        console.log('All Clear');
+    }
+    
+    submit.onclick = function() {
+        startValue.value = 1;
+        endValue.value = 100;
+        fizzValue.value = 3;
+        buzzValue.value = 5;
+        innit();
+        console.log('Generated');
+    }
+};
+
+
+// Reload page on icon click
+
+function refreshPage(){
+    window.location.reload();
 }
 
+// For alerts
+
+// startValue.onkeyup = function (e) {
+//         this.value = startValue.value.replace(/^(0*)/, "");
+//         if (startValue.value >= 1 && startValue.value <= 101) {
+//             this.value = startValue.value;
+//         } else if (startValue.value.length === 0) {
+//             startValue.value = null;
+//         } else {
+//             startValue.value = null;
+//             alert("Please add avalid number! From 1 - 100!");
+//         }
+//         innit();
+//     };
+
+//     endValue.onkeyup = function (e) {
+//         this.value = endValue.value.replace(/^(0*)/, "");
+//         if (endValue.value >= 1 && endValue.value <= 101) {
+//             this.value = endValue.value;
+//         } else if (endValue.value.length === 0) {
+//             endValue.value = null;
+//         } else {
+//             endValue.value = null;
+//             alert("Please add avalid number! From 1 - 100!");
+//         }
+//         innit();
+// };
+
 function onClick() {
-    onClickClear();
-    console.log('Button press');
-    for(let i = 1; i<101; i++){
-        const element = document.createElement('div');
+    onClickReset();
+    for(let i = 1; i < 101; i++) {
+        let element = document.createElement('div');
         element.className="element" + i;
         elements.appendChild(element);
-        if (i % 3 ===0 && i % 5 === 0){
+        if (i % 3 === 0 && i % 5 === 0){
             element.setAttribute("id", "element" +i);
-            element.setAttribute("class", "fizzBuzz");
-            element.innerText="FizzBuzz " + i;
+            element.setAttribute("class", "fizzbuzz");
+            element.innerText = "FizzBuzz " + i;
         }
-        
+
         else if (i % 3 === 0 && i % 5 !== 0) {
             element.setAttribute("id", "element" +i);
             element.setAttribute("class", "fizz");
-            element.innerText="Fizz " + i;
+            element.innerText = "Fizz " + i;
         }
 
         else if (i % 5 === 0 && i % 3 !== 0){
             element.setAttribute("id", "element" +i);
             element.setAttribute("class", "buzz");
-            element.innerText="Buzz " + i;
+            element.innerText = "Buzz " + i;
         }
 
         else if (i % 3 !== 0 && i % 5 !== 0){
             element.setAttribute("id", "element" +i);
             element.setAttribute("class", "empty");
-            element.innerText=i;
+            element.innerText = i;
         }
     }
-}
+};
 
-function onClickClear () {
+function onClickReset () {
     while (elements.firstChild) {
         elements.removeChild(elements.firstChild);
     }
-    console.log('Divs cleared');
-}
+};
 
 function onClickCustom (){
-    onClickClear();
-    console.log('Divs cleared');
-    console.log('Generate custom divs');
-    var startValue = document.getElementById('startValue').value;
-    var endValue = document.getElementById('endValue').value;   
-    var fizzValue = document.getElementById('fizzValue').value;
-    var buzzValue = document.getElementById('buzzValue').value;
-    // var fizzColor = document.getElementById('fizzColor').value;
-    // var buzzColor = document.getElementById('buzzColor').value;
-    // var fizzBuzzColor = document.getElementById('fizzBuzzColor').value;
-    // var noneColor = document.getElementById('noneColor').value;
-    
+    onClickReset();
+    let startValue = document.getElementById('startValue').value;
+    let endValue = document.getElementById('endValue').value;   
+    let fizzValue = document.getElementById('fizzValue').value;
+    let buzzValue = document.getElementById('buzzValue').value;
     console.log('start value - ' + startValue);
     console.log('end value - ' + endValue);
     console.log('fizz value - ' + fizzValue);
     console.log('buzz value - ' + buzzValue);
-    // console.log('fizz color - ' + fizzColor);
-    // console.log('buzz color - ' + buzzColor);
-    // console.log('fizzbuzz color - ' + fizzBuzzColor);
-    // console.log('none color - ' + noneColor);
-    
-    for (startValue; startValue<=endValue; startValue++){
-        const element = document.createElement('div');
-        element.id="element" + startValue;
-        elements.appendChild(element);
 
+    for (startValue; startValue <= endValue; startValue++){
+        let element = document.createElement('div');
+        element.id = "element" + startValue;
+        elements.appendChild(element);
         if (startValue % fizzValue === 0 && startValue % buzzValue === 0){
-            element.setAttribute("id", "element" +startValue);
-            element.setAttribute("class", "fizzBuzz");
-            element.innerText="FizzBuzz " + startValue;
-            // element.style.backgroundColor=fizzBuzzColor;
+            element.setAttribute("id", "element" + startValue);
+            element.setAttribute("class", "fizzbuzz");
+            element.innerText = "FizzBuzz " + startValue;
         }
 
         else if (startValue % fizzValue === 0 && startValue % buzzValue !== 0){
-            element.setAttribute("id", "element" +startValue);
+            element.setAttribute("id", "element" + startValue);
             element.setAttribute("class", "fizz");
-            element.innerText="Fizz " + startValue;
+            element.innerText = "Fizz " + startValue;
         }
 
         else if (startValue % buzzValue === 0 && startValue % fizzValue !== 0){
-            element.setAttribute("id", "element" +startValue);
+            element.setAttribute("id", "element" + startValue);
             element.setAttribute("class", "buzz");
-            element.innerText="Buzz " + startValue;
+            element.innerText = "Buzz " + startValue;
         }
 
         else if (startValue % fizzValue !== 0 && startValue % buzzValue !== 0){
-            element.setAttribute("id", "element" +startValue);
+            element.setAttribute("id", "element" + startValue);
             element.setAttribute("class", "empty");
-            element.innerText=startValue;
+            element.innerText = startValue;
         }
     }
 }
